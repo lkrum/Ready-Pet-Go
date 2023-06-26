709cae86d125b965b36ba626533ee355674ccbca
lkrum <leahkrumholz73@gmail.com>
2023-06-26 11:15:25 -0500

    deleted test folders

--- TESTS/ki'sFolder/server/config/connection.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://rxs291:Never5660217!@classactivities.myu493z.mongodb.net/chatAppDB');
-
-module.exports = mongoose.connection;
